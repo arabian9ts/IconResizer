5fe9521a961aba610319dd65ddcda95c92a59622
arabian9ts <nok14.tm59@gmail.com>
2017-08-31 23:07:08 +0900

    append: resize process

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from PIL import Image
+
+if(len(sys.argv) < 2):
+    print('Usage: $ python main.py <your favo size list)>')
+    quit()
+
+img_path = sys.argv[1]
+size_list = sys.argv[2:]
+origin = Image.open(img_path)
+img_name,ext = os.path.splitext(os.path.basename(sys.argv[1]))
+
+for size in size_list:
+    resized = origin.resize((int(size), int(size)), Image.LANCZOS)
+    export_name = img_name + '_' + size + ext
+    resized.save(export_name)
+    print('exported as ' + export_name)
